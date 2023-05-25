65abe53027600f8fc4e194687c59693e1242540f
WorabordinC <music_stm@outlook.com>
2023-05-25 22:48:24 +0700

    step 17

--- src/main/java/com/in28minute/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minute/springboot/myfirstwebapp/login/LoginController.java
@@ -2,28 +2,27 @@ package com.in28minute.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
+@SessionAttributes("name")
 public class LoginController {
 
-    private AuthenticationService authenticationService;
+    private final AuthenticationService authenticationService;
 
     public LoginController(AuthenticationService authenticationService) {
         super();
         this.authenticationService = authenticationService;
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @GetMapping(value = "/login")
     public String gotoLoginPage() {
         return "login";
     }
 
 
-    @RequestMapping(value = "login", method = RequestMethod.POST)
+    @PostMapping(value = "login")
     //login?name=Ranga RequestParam
     public String gotoWelcomePage(@RequestParam String name,
                                   @RequestParam String password, ModelMap model) {
--- src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -1,9 +1,11 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Welcome Page</title>
 </head>
 <body>
-<div>Welcome to in28minutes</div>
-<div>Your Name: ${name}</div>
+<div>Welcome ${name}</div>
+<hr>
+<div><a href="list-todos">Manage</a> your todos</div>
 </body>
 </html>
